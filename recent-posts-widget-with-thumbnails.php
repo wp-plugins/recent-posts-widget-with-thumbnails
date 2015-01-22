9b9fe3e237d34ad3f26780d847796530032a869a
Hinjiriyo <Hinjiriyo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-22 22:15:03 +0000

    Added options, move CSS to external file
    git-svn-id: https://plugins.svn.wordpress.org/recent-posts-widget-with-thumbnails/trunk@1073679 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- recent-posts-widget-with-thumbnails.php
+++ recent-posts-widget-with-thumbnails.php
@@ -3,7 +3,7 @@
 Plugin Name: Recent Posts Widget With Thumbnails
 Plugin URI:  http://wordpress.org/plugins/recent-posts-widget-with-thumbnails/
 Description: Small and fast plugin to display in the sidebar a list of linked titles and thumbnails of the most recent postings
-Version:     2.2.2
+Version:     2.3
 Author:      Martin Stehle
 Author URI:  http://stehle-internet.de
 Text Domain: recent-posts-thumbnails
@@ -20,11 +20,13 @@ License URI: http://www.gnu.org/licenses/gpl-2.0.txt
 class Recent_Posts_Widget_With_Thumbnails extends WP_Widget {
 
 	var $plugin_slug;  // identifier of this plugin for WP
+	var $plugin_version; // number of current plugin version
 	var $number_posts;  // number of posts to show in the widget
-	var $thumb_width;  // width of the thumbnail
-	var $thumb_height; // height of the thumbnail
-	var $thumb_default_url; // URL of the default thumbnail
+	var $default_thumb_width;  // width of the thumbnail
+	var $default_thumb_height; // height of the thumbnail
+	var $default_thumb_url; // URL of the default thumbnail
 	var $text_domain; // text domain of this plugin
+	var $css_file_path; // path of the public css file
 
 	function __construct() {
 		switch ( get_locale() ) {
@@ -37,11 +39,13 @@ class Recent_Posts_Widget_With_Thumbnails extends WP_Widget {
 				$widget_desc = 'List of your site&#8217;s most recent posts, with clickable title and thumbnails.';
 		}
 		$this->plugin_slug  = 'recent-posts-widget-with-thumbnails';
+		$this->plugin_version  = '2.3';
 		$this->number_posts  = 5;
-		$this->thumb_width  = 55;
-		$this->thumb_height = 55;
-		$this->thumb_default_url = plugins_url( 'default_thumb.gif', __FILE__ );
+		$this->default_thumb_width  = 55;
+		$this->default_thumb_height = 55;
+		$this->default_thumb_url = plugins_url( 'default_thumb.gif', __FILE__ );
 		$this->text_domain = 'recent-posts-thumbnails';
+		$this->css_file_path = plugin_dir_path( __FILE__ ) . 'public.css';
 		
 		$widget_ops = array( 'classname' => $this->plugin_slug, 'description' => $widget_desc );
 		parent::__construct( $this->plugin_slug, $widget_name, $widget_ops );
@@ -50,7 +54,7 @@ class Recent_Posts_Widget_With_Thumbnails extends WP_Widget {
 		add_action( 'save_post', array( $this, 'flush_widget_cache' ) );
 		add_action( 'deleted_post', array( $this, 'flush_widget_cache' ) );
 		add_action( 'switch_theme', array( $this, 'flush_widget_cache' ) );
-		add_action( 'wp_head', array( $this, 'print_list_css' ) );
+		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_public_style' ) );
 	}
 
 	function widget( $args, $instance ) {
@@ -80,10 +84,12 @@ class Recent_Posts_Widget_With_Thumbnails extends WP_Widget {
 		/** This filter is documented in wp-includes/default-widgets.php */
 		$title = apply_filters( 'widget_title', $title, $instance, $this->id_base );
 
-		$number 			= ( ! empty( $instance[ 'number' ] ) ) ? absint( $instance[ 'number' ] ) : $this->number_posts;
-		$thumb_width 		= ( ! empty( $instance[ 'thumb_width' ] ) ) ? absint( $instance[ 'thumb_width' ] ) : $this->thumb_width;
-		$thumb_height 		= ( ! empty( $instance[ 'thumb_height' ] ) ) ? absint( $instance[ 'thumb_height' ] ) : $this->thumb_height;
-		$default_url 		= ( ! empty( $instance[ 'default_url' ] ) ) ? $instance[ 'default_url' ] : $this->thumb_default_url;
+		$number_posts		= ( ! empty( $instance[ 'number_posts' ] ) ) ? absint( $instance[ 'number_posts' ] ) : $this->number_posts;
+		$thumb_width 		= ( ! empty( $instance[ 'thumb_width' ] ) ) ? absint( $instance[ 'thumb_width' ] ) : $this->default_thumb_width;
+		$thumb_height 		= ( ! empty( $instance[ 'thumb_height' ] ) ) ? absint( $instance[ 'thumb_height' ] ) : $this->default_thumb_height;
+		$default_url 		= ( ! empty( $instance[ 'default_url' ] ) ) ? $instance[ 'default_url' ] : $this->default_thumb_url;
+		$keep_aspect_ratio  = isset( $instance[ 'keep_aspect_ratio' ] ) ? $instance[ 'keep_aspect_ratio' ] : false;
+		$hide_title			= isset( $instance[ 'hide_title' ] ) ? $instance[ 'hide_title' ] : false;
 		$show_date 			= isset( $instance[ 'show_date' ] ) ? $instance[ 'show_date' ] : false;
 		$show_thumb 		= isset( $instance[ 'show_thumb' ] ) ? $instance[ 'show_thumb' ] : false;
 		$use_default 		= isset( $instance[ 'use_default' ] ) ? $instance[ 'use_default' ] : false;
@@ -91,10 +97,10 @@ class Recent_Posts_Widget_With_Thumbnails extends WP_Widget {
 		$only_1st_img 		= isset( $instance[ 'only_1st_img' ] ) ? $instance[ 'only_1st_img' ] : false;
 
 		// sanitizes vars
-		if ( ! $number ) 		$number = $this->number_posts;
-		if ( ! $thumb_width )	$thumb_width = $this->thumb_width;
-		if ( ! $thumb_height )	$thumb_height = $this->thumb_height;
-		if ( ! $default_url )	$default_url = $this->thumb_default_url;
+		if ( ! $number_posts ) 	$number_posts = $this->number_posts;
+		if ( ! $thumb_width )	$thumb_width = $this->default_thumb_width;
+		if ( ! $thumb_height )	$thumb_height = $this->default_thumb_height;
+		if ( ! $default_url )	$default_url = $this->default_thumb_url;
 
 		$size 	= array( $thumb_width, $thumb_height );
 
@@ -108,7 +114,7 @@ class Recent_Posts_Widget_With_Thumbnails extends WP_Widget {
 		 * @param array $args An array of arguments used to retrieve the recent posts.
 		 */
 		$r = new WP_Query( apply_filters( 'widget_posts_args', array(
-			'posts_per_page'      => $number,
+			'posts_per_page'      => $number_posts,
 			'no_found_rows'       => true,
 			'post_status'         => 'publish',
 			'ignore_sticky_posts' => true
@@ -165,7 +171,10 @@ class Recent_Posts_Widget_With_Thumbnails extends WP_Widget {
 					endif; // not is_thumb
 					// (else do nothing)
 				endif; // show_thumb
-				get_the_title() ? the_title() : the_ID(); 
+				// show title if wished
+				if ( ! $hide_title ) {
+					get_the_title() ? the_title() : the_ID(); 
+				}
 				?></a><?php 
 				if ( $show_date ) : 
 					?> <span class="post-date"><?php echo get_the_date(); ?></span><?php 
@@ -187,24 +196,36 @@ class Recent_Posts_Widget_With_Thumbnails extends WP_Widget {
 		}
 	}
 
-	function update( $new_instance, $old_instance ) {
-		$instance = $old_instance;
-		$instance[ 'number' ] 		= absint( $new_instance[ 'number' ] );
-		$instance[ 'thumb_width' ] 	= absint( $new_instance[ 'thumb_width' ] );
-		$instance[ 'thumb_height' ] = absint( $new_instance[ 'thumb_height' ] );
-		$instance[ 'title' ] 		= strip_tags( $new_instance[ 'title' ]);
-		$instance[ 'default_url' ] 	= strip_tags( $new_instance[ 'default_url' ]);
-		$instance[ 'show_date' ] 	= isset( $new_instance[ 'show_date' ] ) ? (bool) $new_instance[ 'show_date' ] : false;
-		$instance[ 'show_thumb' ] 	= isset( $new_instance[ 'show_thumb' ] ) ? (bool) $new_instance[ 'show_thumb' ] : false;
-		$instance[ 'use_default' ] 	= isset( $new_instance[ 'use_default' ] ) ? (bool) $new_instance[ 'use_default' ] : false;
-		$instance[ 'try_1st_img' ] 	= isset( $new_instance[ 'try_1st_img' ] ) ? (bool) $new_instance[ 'try_1st_img' ] : false;
-		$instance[ 'only_1st_img' ] = isset( $new_instance[ 'only_1st_img' ] ) ? (bool) $new_instance[ 'only_1st_img' ] : false;
+	function update( $new_widget_settings, $old_widget_settings ) {
+		$instance = $old_widget_settings;
+		// sanitize user input before update
+		$instance[ 'number_posts' ]	= absint( $new_widget_settings[ 'number_posts' ] );
+		$instance[ 'thumb_width' ] 	= absint( $new_widget_settings[ 'thumb_width' ] );
+		$instance[ 'thumb_height' ] = absint( $new_widget_settings[ 'thumb_height' ] );
+		$instance[ 'title' ] 		= strip_tags( $new_widget_settings[ 'title' ]);
+		$instance[ 'default_url' ] 	= strip_tags( $new_widget_settings[ 'default_url' ]);
+		$instance[ 'keep_aspect_ratio' ] = isset( $new_widget_settings[ 'keep_aspect_ratio' ] ) ? (bool) $new_widget_settings[ 'keep_aspect_ratio' ] : false;
+		$instance[ 'hide_title' ] 	= isset( $new_widget_settings[ 'hide_title' ] ) ? (bool) $new_widget_settings[ 'hide_title' ] : false;
+		$instance[ 'show_date' ] 	= isset( $new_widget_settings[ 'show_date' ] ) ? (bool) $new_widget_settings[ 'show_date' ] : false;
+		$instance[ 'show_thumb' ] 	= isset( $new_widget_settings[ 'show_thumb' ] ) ? (bool) $new_widget_settings[ 'show_thumb' ] : false;
+		$instance[ 'use_default' ] 	= isset( $new_widget_settings[ 'use_default' ] ) ? (bool) $new_widget_settings[ 'use_default' ] : false;
+		$instance[ 'try_1st_img' ] 	= isset( $new_widget_settings[ 'try_1st_img' ] ) ? (bool) $new_widget_settings[ 'try_1st_img' ] : false;
+		$instance[ 'only_1st_img' ] = isset( $new_widget_settings[ 'only_1st_img' ] ) ? (bool) $new_widget_settings[ 'only_1st_img' ] : false;
+
+		// empty widget cache
 		$this->flush_widget_cache();
 
 		$alloptions = wp_cache_get( 'alloptions', 'options' );
 		if ( isset( $alloptions[ $this->plugin_slug ]) )
 			delete_option( $this->plugin_slug );
 
+		// delete current css file to let make new one via $this->enqueue_public_style()
+		if ( file_exists( $this->css_file_path ) ) {
+			// remove the file
+			unlink( $this->css_file_path );
+		}
+
+		// return sanitized current widget settings
 		return $instance;
 	}
 
@@ -214,10 +235,12 @@ class Recent_Posts_Widget_With_Thumbnails extends WP_Widget {
 
 	function form( $instance ) {
 		$title        = isset( $instance[ 'title' ] ) ? esc_attr( $instance[ 'title' ] ) : '';
-		$default_url  = isset( $instance[ 'default_url' ] ) ? esc_url( $instance[ 'default_url' ] ) : $this->thumb_default_url;
-		$thumb_width  = isset( $instance[ 'thumb_width' ] )  ? absint( $instance[ 'thumb_width' ] )  : $this->thumb_width;
-		$thumb_height = isset( $instance[ 'thumb_height' ] ) ? absint( $instance[ 'thumb_height' ] ) : $this->thumb_height;
-		$number       = isset( $instance[ 'number' ] ) ? absint( $instance[ 'number' ] ) : $this->number_posts;
+		$default_url  = isset( $instance[ 'default_url' ] ) ? esc_url( $instance[ 'default_url' ] ) : $this->default_thumb_url;
+		$thumb_width  = isset( $instance[ 'thumb_width' ] )  ? absint( $instance[ 'thumb_width' ] )  : $this->default_thumb_width;
+		$thumb_height = isset( $instance[ 'thumb_height' ] ) ? absint( $instance[ 'thumb_height' ] ) : $this->default_thumb_height;
+		$number_posts = isset( $instance[ 'number_posts' ] ) ? absint( $instance[ 'number_posts' ] ) : $this->number_posts;
+		$keep_aspect_ratio = isset( $instance[ 'keep_aspect_ratio' ] ) ? (bool) $instance[ 'keep_aspect_ratio' ] : false;
+		$hide_title   = isset( $instance[ 'hide_title' ] ) ? (bool) $instance[ 'hide_title' ] : false;
 		$show_date    = isset( $instance[ 'show_date' ] ) ? (bool) $instance[ 'show_date' ] : false;
 		$show_thumb   = isset( $instance[ 'show_thumb' ] ) ? (bool) $instance[ 'show_thumb' ] : true;
 		$use_default  = isset( $instance[ 'use_default' ] ) ? (bool) $instance[ 'use_default' ] : false;
@@ -225,17 +248,17 @@ class Recent_Posts_Widget_With_Thumbnails extends WP_Widget {
 		$only_1st_img = isset( $instance[ 'only_1st_img' ] ) ? (bool) $instance[ 'only_1st_img' ] : false;
 		
 		// sanitize vars
-		if ( ! $number ) 		$number = $this->number_posts;
-		if ( ! $thumb_width )	$thumb_width = $this->thumb_width;
-		if ( ! $thumb_height )	$thumb_height = $this->thumb_height;
-		if ( ! $default_url )	$default_url = $this->thumb_default_url;
+		if ( ! $number_posts )	$number_posts = $this->number_posts;
+		if ( ! $thumb_width )	$thumb_width = $this->default_thumb_width;
+		if ( ! $thumb_height )	$thumb_height = $this->default_thumb_height;
+		if ( ! $default_url )	$default_url = $this->default_thumb_url;
 		
 ?>
 		<p><label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
 		<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo $title; ?>" /></p>
 
-		<p><label for="<?php echo $this->get_field_id( 'number' ); ?>"><?php _e( 'Number of posts to show:' ); ?></label>
-		<input id="<?php echo $this->get_field_id( 'number' ); ?>" name="<?php echo $this->get_field_name( 'number' ); ?>" type="text" value="<?php echo $number; ?>" size="3" /></p>
+		<p><label for="<?php echo $this->get_field_id( 'number_posts' ); ?>"><?php _e( 'Number of posts to show:' ); ?></label>
+		<input id="<?php echo $this->get_field_id( 'number_posts' ); ?>" name="<?php echo $this->get_field_name( 'number_posts' ); ?>" type="text" value="<?php echo $number_posts; ?>" size="3" /></p>
 
 		<p><input class="checkbox" type="checkbox" <?php checked( $show_date ); ?> id="<?php echo $this->get_field_id( 'show_date' ); ?>" name="<?php echo $this->get_field_name( 'show_date' ); ?>" />
 		<label for="<?php echo $this->get_field_id( 'show_date' ); ?>"><?php _e( 'Display post date?' ); ?></label></p>
@@ -251,6 +274,12 @@ class Recent_Posts_Widget_With_Thumbnails extends WP_Widget {
 		<p><label for="<?php echo $this->get_field_id( 'thumb_height' ); ?>"><?php _e( 'Height of thumbnail', $this->text_domain ); ?>:</label>
 		<input id="<?php echo $this->get_field_id( 'thumb_height' ); ?>" name="<?php echo $this->get_field_name( 'thumb_height' ); ?>" type="text" value="<?php echo $thumb_height; ?>" size="3" /></p>
 
+		<p><input class="checkbox" type="checkbox" <?php checked( $keep_aspect_ratio ); ?> id="<?php echo $this->get_field_id( 'keep_aspect_ratio' ); ?>" name="<?php echo $this->get_field_name( 'keep_aspect_ratio' ); ?>" />
+		<label for="<?php echo $this->get_field_id( 'keep_aspect_ratio' ); ?>"><?php _e( 'Use aspect ratios of original images?', $this->text_domain ); ?> <em><?php _e( 'If checked the given width is used to determine the height of the thumbnail automatically. This option also supports responsive web design.', $this->text_domain ); ?></em></label></p>
+		
+		<p><input class="checkbox" type="checkbox" <?php checked( $hide_title ); ?> id="<?php echo $this->get_field_id( 'hide_title' ); ?>" name="<?php echo $this->get_field_name( 'hide_title' ); ?>" />
+		<label for="<?php echo $this->get_field_id( 'hide_title' ); ?>"><?php _e( 'Do not show title?', $this->text_domain ); ?> <em><?php _e( 'Make sure you set a default thumbnail for posts without a thumbnail, otherwise there will be no link.', $this->text_domain ); ?></em></label> </p>
+		
 		<p><input class="checkbox" type="checkbox" <?php checked( $try_1st_img ); ?> id="<?php echo $this->get_field_id( 'try_1st_img' ); ?>" name="<?php echo $this->get_field_name( 'try_1st_img' ); ?>" />
 		<label for="<?php echo $this->get_field_id( 'try_1st_img' ); ?>"><?php _e( "Try to use the post's first image if post has no featured image?", $this->text_domain ); ?></label></p>
 		
@@ -268,32 +297,26 @@ class Recent_Posts_Widget_With_Thumbnails extends WP_Widget {
 	}
 	
 	/**
-	 * Print the widget's CSS in the HEAD section of the frontend
+	 * Load the widget's CSS in the HEAD section of the frontend
 	 *
-	 * @since 1.0
+	 * @since 2.3
 	 */
-	function print_list_css () {
-		$options = get_option( 'widget_' . $this->plugin_slug );
-		// if options does not exist
-		if ( false === $options ) :
-			// use default values
-			$width = $this->thumb_width;
-			$height = $this->thumb_height;
-		// else use stored values
-		else :	
-			$options = array_shift( $options );
-			$width  = absint( $options[ 'thumb_width' ]  );
-			$height = absint( $options[ 'thumb_height' ] );
-			if ( ! $width )  $width = $this->thumb_width;
-			if ( ! $height ) $height = $this->thumb_height;
-		endif;
-		print '<style type="text/css">';
-		print "\n";
-		print '.recent-posts-widget-with-thumbnails ul li { overflow: hidden; font-size: 91%; margin: 0 0 1.5em; }';
-		print "\n";
-		printf ('.recent-posts-widget-with-thumbnails ul li img { display: inline; float: left; margin: .3em .75em .75em 0; width: %dpx; height: %dpx; }', $width, $height );
-		print "\n";
-		print '</style>';
+	public function enqueue_public_style () {
+		// load style only in frontend
+		if ( is_admin() ) return;
+		// make sure the css file exists
+		if ( ! file_exists( $this->css_file_path ) ) {
+			// make the file
+			$this->make_css_file();
+		}
+		// enqueue the style
+		wp_enqueue_style(
+			$this->plugin_slug . '-public-style',
+			plugin_dir_url( __FILE__ ) . 'public.css',
+			array(),
+			$this->plugin_version,
+			'all' 
+		);
 	}
 
 	/**
@@ -379,6 +402,59 @@ class Recent_Posts_Widget_With_Thumbnails extends WP_Widget {
 			return false;
 		endif; // thumb_id
 	}
+
+	/**
+	 * Generate the css file with stored settings
+	 *
+	 * @since 2.3
+	 */
+	private function make_css_file () {
+
+		// get stored settings
+		$all_instances = $this->get_settings();
+
+		// generate CSS
+		$css_code = sprintf( '.%s ul li { overflow: hidden; margin: 0 0 1.5em; }', $this->plugin_slug );
+		$css_code .= "\n"; 
+		$css_code .= sprintf( '.%s ul li img { display: inline; float: left; margin: .3em .75em .75em 0; }', $this->plugin_slug );
+		$css_code .= "\n";
+		$set_default = true;
+		foreach ( $all_instances as $number => $settings ) {
+			if ( isset( $settings[ 'thumb_width' ] ) ) {
+				$width  = absint( $settings[ 'thumb_width' ]  );
+				if ( ! $width ) $width = $this->default_thumb_width;
+			}
+			if ( isset( $settings[ 'thumb_height' ] ) ) {
+				$height = absint( $settings[ 'thumb_height' ] );
+				if ( ! $height ) $height = $this->default_thumb_height;
+			}
+			$keep_aspect_ratio = false;
+			if ( isset( $settings[ 'keep_aspect_ratio' ] ) ) {
+				$keep_aspect_ratio = (bool) $settings[ 'keep_aspect_ratio' ];
+			}
+			if ( $keep_aspect_ratio ) {
+				$css_code .= sprintf( '#%s-%d img { max-width: %dpx; width: 100%%; height: auto; }', $this->plugin_slug, $number, $width );
+				$css_code .= "\n"; 
+			} else {
+				$css_code .= sprintf( '#%s-%d img { width: %dpx; height: %dpx; }', $this->plugin_slug, $number, $width, $height );
+				$css_code .= "\n"; 
+			}
+			$set_default = false;
+		}
+		// set at least this statement if no settings are stored
+		if ( $set_default ) {
+			$css_code .= sprintf( '.%s img { width: %dpx; height: %dpx; }', $this->plugin_slug, $this->default_thumb_width, $this->default_thumb_height );
+			$css_code .= "\n"; 
+		}
+		
+		// write file
+		$fh = fopen( $this->css_file_path, 'w' );
+		#if ( $fhd ) {
+			fwrite ( $fh, $css_code );
+			fclose( $fh );
+		#}
+	}
+
 }
 
 /**
